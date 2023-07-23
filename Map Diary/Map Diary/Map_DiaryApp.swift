d68908577b8e993cab2c91d2837fd997c01b829c
Christopher Tang <ctang127@terpmail.umd.edu>
2023-07-23 17:29:08 -0400

    Initial Commit

---
+++ Map Diary/Map Diary/Map_DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_DiaryApp.swift
+//  Map Diary
+//
+//  Created by Christopher Tang on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Map_DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
